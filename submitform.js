0ad575106ff93f80e582ab673fe7fbce3538e6a2
Vinh <100789255+Inustur@users.noreply.github.com>
2023-10-05 16:07:47 -0600

    Adding thankyou.html
    adding thank you page & adjusting on submitform.js

--- submitform.js
+++ submitform.js
@@ -1,14 +1,36 @@
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let comment = document.getElementById("comment");
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.getElementById("submission");
+  const formStatus = contactForm.querySelector(".form-status");
 
-function submit(){
-if (name.value == "" || email.value == "") {
-    alert("Ensure you input a value in both fields!");
-} else {
-    // perform operation with form input
-    alert("Received!! Thank you for your time.");
-  }
-};
+  contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-window.addEventListener("submit",submit);
+    // Display a loading message while processing the form
+    formStatus.textContent = "Sending...";
+
+    // Create a new FormData object from the form
+    const formData = new FormData(contactForm);
+
+    // Send a POST request to the form's action URL using Fetch API
+    fetch(contactForm.action, {
+      method: "POST",
+      body: formData,
+    })
+      .then(function (response) {
+        // Check the response status
+        if (response.ok) {
+          // Redirect to thankyou.html upon successful submission
+          window.location.href = "thankyou.html";
+        } else {
+          // Show error message
+          formStatus.textContent = "Failed to send message. Please try again.";
+          formStatus.classList.add("error");
+        }
+      })
+      .catch(function (error) {
+        // Show error message
+        formStatus.textContent = "An error occurred. Please try again later.";
+        formStatus.classList.add("error");
+      });
+  });
+});
